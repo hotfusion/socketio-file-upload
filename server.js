e6df01966c52801a4cf852f8287f6187f31e4245
hotfusion <116390360+hotfusion@users.noreply.github.com>
2023-06-27 19:37:38 -0700

    Update server.js

--- server.js
+++ server.js
@@ -37,17 +37,6 @@ var util = require("util"),
 
 function SocketIOFileUploadServer(options) {
 	"use strict";
-
-	// worker support
-	if(!window && FileReader && File)
-		var window = {
-		      FileReader,File
-		};
-
-	 if(!document)
-		 var document = {
-		    createEvent:(name) => new Event(name)
-		 };
 	
 	EventEmitter.call(this);
 	var self = this; // avoids context issues
